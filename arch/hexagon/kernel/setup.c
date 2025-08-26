32491fd79cb4d4a949785510e1e7eb2c3c360de7
Richard Kuo <rkuo@quicinc.com>
2025-08-26 00:10:47 -0500

    hexagon: add SM8150 platform support
    Add platform support for SM8150 board including board setup code,
    v66 debug support and default kernel configurations.
    
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- arch/hexagon/kernel/setup.c
+++ arch/hexagon/kernel/setup.c
@@ -22,6 +22,7 @@
 #include <asm/platform.h>
 #include <asm/prom.h>
 #include <asm/time.h>
+#include <asm/angel_console.h>
 #include <linux/percpu.h>
 
 #ifdef CONFIG_SMP
@@ -53,6 +54,8 @@ void __init setup_arch(char **cmdline_p)
 	 * Set up event bindings to handle exceptions and interrupts.
 	 */
 	__vmsetvec(_K_VM_event_vector);
+
+	register_angel_console();
 
 	printk(KERN_INFO "PHYS_OFFSET=0x%08lx\n", PHYS_OFFSET);
 	/*  initial machine setup from flattened device tree  */
