b647bceab8f446cb52c051fd214d0873ed9d67ce
Richard Kuo <rkuo@quicinc.com>
2025-10-13 20:55:14 -0500

    hexagon: updates to module loader
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

---
+++ arch/hexagon/kernel/reloc.h
@@ -0,0 +1,25 @@
+/*
+ * Kernel module loader support routines for Hexagon
+ *
+ * Copyright (c) 2010-2017, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+#ifndef RELOC_H
+#define RELOC_H
+
+extern uint32_t do_reloc(uint32_t reloc_type, uint32_t insn, uint32_t value);
+
+#endif
