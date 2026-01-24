491820db2a2f9fe6d6ab8c34452578ee7ddbf765
Brian Cain <brian.cain@oss.qualcomm.com>
2026-01-24 16:38:42 -0600

    hexagon: update memory management headers
    Update memory layout, MMU context, page table, and page allocation
    headers for improved memory management support.
    
    Signed-off-by: Richard Kuo <rkuo@quicinc.com>
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- arch/hexagon/include/asm/mem-layout.h
+++ arch/hexagon/include/asm/mem-layout.h
@@ -16,7 +16,15 @@
  * assign it to the location counter.
  */
 
+#ifdef CONFIG_HEXAGON_SPLIT_2GB
+#define PAGE_OFFSET			_AC(0x80000000, UL)
+#else
 #define PAGE_OFFSET			_AC(0xc0000000, UL)
+#endif
+
+#ifndef ELF_LOAD_ADDRESS
+#define ELF_LOAD_ADDRESS			_AC(0x00000000, UL)
+#endif
 
 /*
  * Compiling for a platform that needs a crazy physical offset
@@ -40,7 +48,6 @@ extern unsigned long	__phys_offset;
 
 #define TASK_SIZE			(PAGE_OFFSET)
 
-/*  not sure how these are used yet  */
 #define STACK_TOP			TASK_SIZE
 #define STACK_TOP_MAX			TASK_SIZE
 
@@ -102,6 +109,5 @@ extern int max_kernel_seg;
  */
 #define VMALLOC_END (PKMAP_BASE-PAGE_SIZE*2)
 #endif /*  !__ASSEMBLY__  */
-
 
 #endif /* _ASM_HEXAGON_MEM_LAYOUT_H */
