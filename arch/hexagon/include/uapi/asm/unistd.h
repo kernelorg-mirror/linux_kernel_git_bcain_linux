a16ebf95f8253bfbd74cf22756c3a84078503c81
Brian Cain <brian.cain@oss.qualcomm.com>
2026-01-24 16:39:31 -0600

    hexagon: update UAPI headers
    Update user-space API headers including registers, syscall numbers,
    user struct, and signal context definitions.
    
    Signed-off-by: Richard Kuo <rkuo@quicinc.com>
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- arch/hexagon/include/uapi/asm/unistd.h
+++ arch/hexagon/include/uapi/asm/unistd.h
@@ -38,3 +38,8 @@
 #define __ARCH_WANT_TIME32_SYSCALLS
 
 #include <asm-generic/unistd.h>
+
+//  Prevent uClibc from using this.
+//  Would preferably just not define __ARCH_WANT_SYSCALL_DEPRECATED, but
+//  we're not there yet.
+#undef __NR_vfork
