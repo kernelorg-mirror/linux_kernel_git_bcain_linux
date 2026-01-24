ebbdd4baf8f97ef61b90144f07515fc4c4a15977
Brian Cain <brian.cain@oss.qualcomm.com>
2026-01-24 16:38:33 -0600

    hexagon: update VM interface header
    Update hexagon_vm.h with new VM interface definitions, hypercalls,
    and support for newer hexagon architecture versions.
    
    Co-authored-by: Richard Kuo <rkuo@quicinc.com>
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- arch/hexagon/kernel/hexagon_ksyms.c
+++ arch/hexagon/kernel/hexagon_ksyms.c
@@ -6,6 +6,7 @@
  */
 
 #include <linux/dma-mapping.h>
+#include <linux/export.h>
 #include <asm/hexagon_vm.h>
 #include <asm/io.h>
 #include <linux/uaccess.h>
@@ -15,8 +16,11 @@ EXPORT_SYMBOL(__clear_user_hexagon);
 EXPORT_SYMBOL(raw_copy_from_user);
 EXPORT_SYMBOL(raw_copy_to_user);
 EXPORT_SYMBOL(__vmgetie);
-EXPORT_SYMBOL(__vmsetie);
+EXPORT_SYMBOL(vmgetie_cached);
+EXPORT_SYMBOL(vmsetie_cached);
 EXPORT_SYMBOL(__vmyield);
+EXPORT_SYMBOL(__vmhwconfig);
+EXPORT_SYMBOL(empty_zero_page);
 EXPORT_SYMBOL(memcpy);
 EXPORT_SYMBOL(memset);
 
