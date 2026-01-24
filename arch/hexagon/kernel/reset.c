67b2ae5b4e028d04971575e807a0984c97e7daa5
Brian Cain <brian.cain@oss.qualcomm.com>
2026-01-24 16:40:37 -0600

    hexagon: update reset and stacktrace
    Update system reset handling and stack trace support.
    
    Co-authored-by: Richard Kuo <rkuo@quicinc.com>
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- arch/hexagon/kernel/reset.c
+++ arch/hexagon/kernel/reset.c
@@ -5,21 +5,33 @@
 
 #include <linux/reboot.h>
 #include <linux/smp.h>
+#include <linux/irqflags.h>
 #include <asm/hexagon_vm.h>
+
+static inline void __do_vmstop(void *info) {
+	__vmstop((long)info);
+}
 
 void machine_power_off(void)
 {
-	smp_send_stop();
-	__vmstop();
+	on_each_cpu(__do_vmstop, (void *)poweroff, 0);
 }
 
 void machine_halt(void)
 {
+	on_each_cpu(__do_vmstop, (void *)halt, 0);
 }
 
 void machine_restart(char *cmd)
 {
+	on_each_cpu(__do_vmstop, (void *)restart, 0);
 }
 
 void (*pm_power_off)(void) = NULL;
 EXPORT_SYMBOL(pm_power_off);
+
+static void hexagon_pm_power_off(void)
+{
+	on_each_cpu(__do_vmstop, (void *)poweroff, 0);
+}
+
