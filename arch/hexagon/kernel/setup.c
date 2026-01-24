af437bfe6b48c6b7436b42f989f7eeb4afc5cfbc
Brian Cain <brian.cain@oss.qualcomm.com>
2026-01-24 16:39:41 -0600

    hexagon: update boot and setup code
    Update head.S boot code, setup.c system initialization, asm-offsets,
    and linker script for improved boot sequence and memory layout.
    
    Co-authored-by: Richard Kuo <rkuo@quicinc.com>
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- arch/hexagon/kernel/setup.c
+++ arch/hexagon/kernel/setup.c
@@ -19,17 +19,25 @@
 #include <asm/processor.h>
 #include <asm/hexagon_vm.h>
 #include <asm/vm_mmu.h>
+#include <asm/platform.h>
+#include <asm/prom.h>
 #include <asm/time.h>
+#include <linux/percpu.h>
+
+#ifdef CONFIG_SMP
+DECLARE_PER_CPU(u32, vpid);
+#else
+DEFINE_PER_CPU(u32, vpid);
+#endif
+
+unsigned long vmversion;
 
 char cmd_line[COMMAND_LINE_SIZE];
 static char default_command_line[COMMAND_LINE_SIZE] __initdata = CONFIG_CMDLINE;
 
-int on_simulator;
+void *boot_info;
 
-void calibrate_delay(void)
-{
-	loops_per_jiffy = thread_freq_mhz * 1000000 / HZ;
-}
+struct machine_desc *mdesc;
 
 /*
  * setup_arch -  high level architectural setup routine
@@ -38,15 +46,8 @@ void calibrate_delay(void)
 
 void __init setup_arch(char **cmdline_p)
 {
-	char *p = &external_cmdline_buffer;
-
-	/*
-	 * These will eventually be pulled in via either some hypervisor
-	 * or devicetree description.  Hardwiring for now.
-	 */
-	pcycle_freq_mhz = 600;
-	thread_freq_mhz = 100;
-	sleep_clk_freq = 32000;
+	char *p = &external_buffer;
+	void *dtb = &__dtb_start;
 
 	/*
 	 * Set up event bindings to handle exceptions and interrupts.
@@ -54,23 +55,45 @@ void __init setup_arch(char **cmdline_p)
 	__vmsetvec(_K_VM_event_vector);
 
 	printk(KERN_INFO "PHYS_OFFSET=0x%08lx\n", PHYS_OFFSET);
+	/*  initial machine setup from flattened device tree  */
+	mdesc = setup_machine_fdt(dtb);
 
+	if (!mdesc)
+		panic("setup_machine_fdt returned NULL\n");
+
+	if (mdesc->setup_arch_platform) {
+		mdesc->setup_arch_platform();
+	}
+
+	printk("vmversion=0x%08lx\n", vmversion);
+	printk("vm build id=0x%08lx\n", __vmgetinfo(vm_info_build_id));
+	printk("boot_info=%p\n", boot_info);
 	/*
-	 * Simulator has a few differences from the hardware.
-	 * For now, check uninitialized-but-mapped memory
-	 * prior to invoking setup_arch_memory().
+	 * Will need to work on boot specification.
 	 */
-	if (*(int *)((unsigned long)_end + 8) == 0x1f1f1f1f)
-		on_simulator = 1;
-	else
-		on_simulator = 0;
+	//  Todo:  seriously need to fix this for all platforms
+#ifdef CONFIG_HEXAGON_MSM8960_FLUID
+	{
+		unsigned int *magic;
 
-	if (p[0] != '\0')
+		p = NULL;
+		for (magic = (unsigned int *)&external_buffer;
+		     magic < ((unsigned int *)&external_buffer) + 32; magic++) {
+			if (*magic == 0x54410009) {
+				p = (char *)++magic;
+				break;
+			}
+		}
+	}
+#endif
+
+#ifndef CONFIG_HEXAGON_MSM8974_FLUID
+	if (p && (p[0] != '\0'))
 		strscpy(boot_command_line, p, COMMAND_LINE_SIZE);
 	else
 		strscpy(boot_command_line, default_command_line,
 			COMMAND_LINE_SIZE);
-
+#endif
 	/*
 	 * boot_command_line and the value set up by setup_arch
 	 * are both picked up by the init code. If no reason to
@@ -83,9 +106,18 @@ void __init setup_arch(char **cmdline_p)
 
 	setup_arch_memory();
 
+	/*  Now is time we unflatten devicetree  */
+	unflatten_device_tree();
+
 #ifdef CONFIG_SMP
 	smp_start_cpus();
 #endif
+
+#if defined(CONFIG_DUMMY_CONSOLE)
+	if (!conswitchp)
+		conswitchp = &dummy_con;
+#endif
+
 }
 
 /*
@@ -115,16 +147,71 @@ static int show_cpuinfo(struct seq_file *m, void *v)
 {
 	int cpu = (unsigned long) v - 1;
 
+#ifdef CONFIG_H2
+	unsigned long		build_id;
+	struct vm_boot_flags	bootflags;
+	struct vm_stlb_info	stlb_info;
+	struct vm_syscfg	syscfg;
+	struct vm_rev		rev;
+	unsigned long		l2size;
+	unsigned long		l2cache;
+	unsigned long		l2tcm;
+	unsigned long		l2tcm_base;
+#endif
+
 #ifdef CONFIG_SMP
 	if (!cpu_online(cpu))
 		return 0;
 #endif
 
-	seq_printf(m, "processor\t: %d\n", cpu);
-	seq_printf(m, "model name\t: Hexagon Virtual Machine\n");
-	seq_printf(m, "BogoMips\t: %lu.%02lu\n",
-		(loops_per_jiffy * HZ) / 500000,
-		((loops_per_jiffy * HZ) / 5000) % 100);
+
+#ifdef CONFIG_H2
+	if (cpu == 0) {
+		//  Break this out elsewhere?
+		build_id	= __vmgetinfo(vm_info_build_id);
+		bootflags.raw	= __vmgetinfo(vm_info_boot_flags);
+		stlb_info.raw	= __vmgetinfo(vm_info_stlb);
+		syscfg.raw	= __vmgetinfo(vm_info_syscfg);
+		rev.raw		= __vmgetinfo(vm_info_rev);
+		l2size		= __vmgetinfo(vm_info_l2mem_size);
+		l2tcm		= __vmgetinfo(vm_info_tcm_size);
+		l2tcm_base	= __vmgetinfo(vm_info_tcm_base);
+		l2cache		= __vmgetinfo(vm_info_l2tag_size);
+
+		//  Macro might be nice
+		seq_printf(m, "VM Build ID\t\t: 0x%08lx\n", build_id);
+		seq_printf(m, "VM Boot flags\t\t: 0x%08lx\n", bootflags.raw);
+		seq_printf(m, "   use_tcm\t\t: %d\n", bootflags.use_tcm);
+		seq_printf(m, "VM STLB Info\t\t: 0x%08lx\n", stlb_info.raw);
+		seq_printf(m, "   enabled\t\t: %d\n", stlb_info.enabled);
+		seq_printf(m, "   max_sets_log2\t: %d\n", stlb_info.max_sets_log2);
+		seq_printf(m, "   max_ways\t\t: %d\n", stlb_info.max_ways);
+		seq_printf(m, "   size\t\t\t: %d\n", stlb_info.size);
+		seq_printf(m, "VM REV\t\t\t: 0x%08lx\n", rev.raw);
+		seq_printf(m, "   isa\t\t\t: %d\n", rev.isa);
+		seq_printf(m, "   l2tags\t\t: %d\n", rev.l2tags);
+		seq_printf(m, "   l2size\t\t: %d\n", rev.l2size);
+		seq_printf(m, "   layer\t\t: %d\n", rev.layer);
+		seq_printf(m, "   metal\t\t: %d\n", rev.metal);
+
+		seq_printf(m, "VM SYSCFG\t\t: 0x%08lx\n", syscfg.raw);
+		seq_printf(m, "   L2 Mem\t\t: %lu\n", l2size);
+		seq_printf(m, "   L2 Cache\t\t: %lu\n", l2cache);
+		seq_printf(m, "   TCM\t\t\t: %lu\n", l2tcm);
+		seq_printf(m, "   TCM Base\t\t: 0x%lx\n", l2tcm_base);
+		seq_printf(m, "\n");
+	}
+#endif
+
+	seq_printf(m, "processor\t\t: %d\n", cpu);
+	seq_printf(m, "model name\t\t: Hexagon Virtual Machine\n");
+#ifdef CONFIG_HEXAGON_MSS
+	seq_printf(m, "subsystem\t\t: MSS\n");
+#endif
+#ifdef CONFIG_HEXAGON_FORCE_USR
+	seq_printf(m, "Forced USR Value\t: 0x%08x\n", CONFIG_HEXAGON_USR_VAL);
+#endif
+	seq_printf(m, "VPID:  0x%08x\n", per_cpu(vpid, cpu));
 	seq_printf(m, "\n");
 	return 0;
 }
@@ -135,3 +222,11 @@ const struct seq_operations cpuinfo_op = {
 	.stop   = &c_stop,
 	.show   = &show_cpuinfo,
 };
+
+static int __init stahp(char *str)
+{
+	asm volatile("brkpt;\n");
+	return 0;
+}
+
+early_param("stahp", stahp);
