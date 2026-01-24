f002f922ed4e8e1e79f144c10460d3fbcf203d86
Brian Cain <brian.cain@oss.qualcomm.com>
2026-01-24 16:38:33 -0600

    hexagon: update VM interface header
    Update hexagon_vm.h with new VM interface definitions, hypercalls,
    and support for newer hexagon architecture versions.
    
    Co-authored-by: Richard Kuo <rkuo@quicinc.com>
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- arch/hexagon/include/asm/hexagon_vm.h
+++ arch/hexagon/include/asm/hexagon_vm.h
@@ -38,6 +38,21 @@
 #define HVM_TRAP1_VMSETREGS		21
 #define HVM_TRAP1_VMGETREGS		22
 #define HVM_TRAP1_VMTIMEROP		24
+#define HVM_TRAP1_VMPMUCONFIG		25
+#define HVM_TRAP1_VMGETINFO		26
+
+#define VM_CLOBBERLIST_ABIV2 "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8",\
+	"r9", "r10", "r11", "r12", "r13", "r14", "r15", "r28", "r31", "sa0",\
+	"lc0", "sa1", "lc1", "m0", "m1", "usr", "p0", "p1", "p2", "p3"
+
+#define VM_CLOBBERLIST_ABIV1 VM_CLOBBERLIST_ABIV2, "r16", "r17", "r18",\
+	"r19", "r20", "r21", "r22", "r23"
+
+#if __HEXAGON_ARCH__ >= 3
+#define VM_CLOBBERLIST VM_CLOBBERLIST_ABIV2
+#else
+#define VM_CLOBBERLIST VM_CLOBBERLIST_ABIV1
+#endif
 
 #ifndef __ASSEMBLY__
 
@@ -65,23 +80,199 @@ enum VM_INT_OPS {
 	hvmi_clear
 };
 
+enum VM_TIMER_OPS {
+	getfreq,
+	getres,
+	gettime,
+	gettimeout,
+	settimeout,
+	deltatimeout
+};
+
+enum VM_STOP_STATUS {
+	none,
+	poweroff,
+	halt,
+	restart,
+	machinecheck
+};
+
+enum VM_INFO_TYPE {
+        vm_info_build_id,
+        vm_info_boot_flags,
+        vm_info_stlb,
+        vm_info_syscfg,
+	vm_info_livelock,
+	vm_info_rev,
+	vm_info_ssbase,
+	vm_info_tlb_free,
+	vm_info_tlb_size,
+	vm_info_physaddr,
+	vm_info_tcm_base,
+	vm_info_l2mem_size,
+	vm_info_tcm_size,
+	vm_info_h2k_pgsize,
+	vm_info_h2k_npages,
+	vm_info_l2vic_base,
+	vm_info_timer_base,
+	vm_info_timer_int,
+	vm_info_error,
+	vm_info_hthreads,
+	vm_info_l2tag_size,
+	vm_info_l2cfg_base,
+	vm_info_clade_base,
+	vm_info_cfgbase,
+	vm_info_hvx_vlength,
+	vm_info_hvx_contexts,
+	vm_info_hvx_switch,
+        vm_info_max
+};
+
+typedef enum {
+	HWCONFIG_L2CACHE,
+	HWCONFIG_PARTITIONS,
+	HWCONFIG_PREFETCH,
+	HWCONFIG_EXTBITS,
+	HWCONFIG_VLENGTH,
+	HWCONFIG_EXTPOWER,
+	HWCONFIG_GETL2REG,
+	HWCONFIG_SETL2REG,
+	HWCONFIG_L2LOCKA,
+	HWCONFIG_L2UNLOCK,
+	HWCONFIG_HWINTOP,
+	HWCONFIG_RESERVED_00,
+	HWCONFIG_RESERVED_01,
+	HWCONFIG_HWTHREADS_NUM,
+	HWCONFIG_HWTHREADS_MASK,
+	HWCONFIG_RESERVED_02,
+	HWCONFIG_RESERVED_03,
+	HWCONFIG_MAX
+} hwconfig_type_t;
+
+struct vm_syscfg {
+	union {
+		unsigned long raw;
+		struct {
+			unsigned long m:1;
+			unsigned long i:1;
+			unsigned long d:1;
+			unsigned long t:1;
+			unsigned long g:1;
+			unsigned long r:1;
+			unsigned long c:1;
+
+			unsigned long unused4:1;
+
+			unsigned long ida:1;
+			unsigned long pm:1;
+
+			unsigned long te:1;
+			unsigned long tl:1;
+			unsigned long kl:1;
+
+			unsigned long bq:1;
+
+			unsigned long unused3:1;
+
+			unsigned long dmt:1;
+
+
+			unsigned long l2cfg:3;
+
+			unsigned long itcm:1;
+
+			unsigned long unused2:1;
+
+			unsigned long l2nwa:1;
+			unsigned long l2nra:1;
+
+			unsigned long l2wb:1;
+			unsigned long l2p:1;
+
+			unsigned long l1dp:2;
+			unsigned long l1ip:2;
+			unsigned long l2part:2;
+			unsigned long unused:1;
+		};
+	};
+};
+
+struct vm_rev {
+	union {
+		unsigned long raw;
+		struct {
+			unsigned long isa:8;
+			unsigned long l2tags:4;
+			unsigned long l2size:4;
+			unsigned long layer:4;
+			unsigned long unused:4;
+			unsigned long metal:8;
+		};
+	};
+};
+
+
+
+struct vm_boot_flags {
+	union {
+		unsigned long raw;
+		struct {
+			unsigned long use_tcm:1;
+			unsigned long unused:31;
+		};
+	};
+};
+
+struct vm_stlb_info {
+	union {
+		unsigned long raw;
+		struct {
+			unsigned long max_sets_log2:8;
+			unsigned long max_ways:8;
+			unsigned long size:8;
+			unsigned long unused:7;
+			unsigned long enabled:1;
+		};
+	};
+};
+
 extern void _K_VM_event_vector(void);
 
 void __vmrte(void);
 long __vmsetvec(void *);
-long __vmsetie(long);
+
+void load_ie_cache(void);
+long vmgetie_cached(void);
+long vmsetie_cached(long);
+long __vmsetie_cached(long, long *);
+void clear_ie_cached(void);
+
 long __vmgetie(void);
 long __vmintop(enum VM_INT_OPS, long, long, long, long);
 long __vmclrmap(void *, unsigned long);
-long __vmnewmap(void *);
+long __vmnewmap(void *, unsigned long type, unsigned long tlb_flush_flag);
 long __vmcache(enum VM_CACHE_OPS op, unsigned long addr, unsigned long len);
 unsigned long long __vmgettime(void);
 long __vmsettime(unsigned long long);
-long __vmstart(void *, void *);
-void __vmstop(void);
+long __vmstart(void *, void *, int relprio);
+void __vmstop(enum VM_STOP_STATUS);
 long __vmwait(void);
 void __vmyield(void);
 long __vmvpid(void);
+unsigned long __vmgetinfo(unsigned long);
+
+/*  I think arg2 can be signed...  */
+unsigned long __vmpmucfg(unsigned long operation, unsigned long r1, long r2, unsigned long r3);
+
+/*  Totally dangerous HWCONFIG  */
+
+long __vmhwconfig(unsigned long, unsigned long, unsigned long, unsigned long);
+
+#ifdef CONFIG_H2
+unsigned long long __vmtimerop(enum VM_TIMER_OPS op, unsigned long dummy, unsigned long long timeout);
+#endif
+
+unsigned long __vmversion(void);
 
 static inline long __vmcache_ickill(void)
 {
@@ -111,7 +302,11 @@ static inline long __vmcache_icinva(unsigned long addr, unsigned long len)
 static inline long __vmcache_idsync(unsigned long addr,
 					   unsigned long len)
 {
+#ifdef CONFIG_HEXAGON_NOCACHESYNC
+		return 1;
+#else
 	return __vmcache(hvmc_idsync, addr, len);
+#endif
 }
 
 static inline long __vmcache_fetch_cfg(unsigned long val)
@@ -166,9 +361,9 @@ static inline long __vmintop_status(long i)
 	return __vmintop(hvmi_status, i, 0, 0, 0);
 }
 
-static inline long __vmintop_post(long i)
+static inline long __vmintop_post(long i, long cpu)
 {
-	return __vmintop(hvmi_post, i, 0, 0, 0);
+	return __vmintop(hvmi_post, i, cpu, 0, 0);
 }
 
 static inline long __vmintop_clear(long i)
@@ -249,8 +444,9 @@ static inline long __vmintop_clear(long i)
 
 #define HVM_GE_C_BUS	0x01
 #define HVM_GE_C_XPROT	0x11
-#define HVM_GE_C_XUSER	0x14
+#define HVM_GE_C_XUSER	0x12
 #define HVM_GE_C_INVI	0x15
+#define HVM_GE_C_COPROC 0x16
 #define HVM_GE_C_PRIVI	0x1B
 #define HVM_GE_C_XMAL	0x1C
 #define HVM_GE_C_WREG	0x1D
@@ -262,6 +458,13 @@ static inline long __vmintop_clear(long i)
 #define HVM_GE_C_RUSER	0x24
 #define HVM_GE_C_WUSER	0x25
 #define HVM_GE_C_CACHE	0x28
+
+/* Extended TLB miss cause codes; earlier ones might be deprecated */
+#define HVM_GE_C_TLBMISSX_0		0x60
+#define HVM_GE_C_TLBMISSX_1		0x61
+#define HVM_GE_C_TLBMISSX_ICINVA	0x62
+#define HVM_GE_C_TLBMISSR		0x70
+#define HVM_GE_C_TLBMISSW		0x71
 
 /*
  * Cause codes for Machine Check
