833b9f9d483b26f9119d9b231ea9726fa69e5857
Brian Cain <brian.cain@oss.qualcomm.com>
2026-01-24 16:40:29 -0600

    hexagon: update module loading and exports
    Update module loading with improved relocation handling and
    update kernel symbol exports.
    
    Signed-off-by: Richard Kuo <rkuo@quicinc.com>
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- arch/hexagon/kernel/hexagon_ksyms.c
+++ arch/hexagon/kernel/hexagon_ksyms.c
@@ -6,8 +6,10 @@
  */
 
 #include <linux/dma-mapping.h>
+#include <linux/export.h>
 #include <asm/hexagon_vm.h>
 #include <asm/io.h>
+#include <asm/notify.h>
 #include <linux/uaccess.h>
 
 /* Additional functions */
@@ -16,14 +18,16 @@ EXPORT_SYMBOL(raw_copy_from_user);
 EXPORT_SYMBOL(raw_copy_to_user);
 EXPORT_SYMBOL(__iounmap);
 EXPORT_SYMBOL(__strnlen_user);
-EXPORT_SYMBOL(__vmgetie);
-EXPORT_SYMBOL(__vmsetie);
+EXPORT_SYMBOL(vmgetie_cached);
+EXPORT_SYMBOL(vmsetie_cached);
 EXPORT_SYMBOL(__vmyield);
+EXPORT_SYMBOL(__vmhwconfig);  //  yikes
 EXPORT_SYMBOL(empty_zero_page);
 EXPORT_SYMBOL(ioremap_nocache);
 EXPORT_SYMBOL(memcpy);
 EXPORT_SYMBOL(memset);
-
+EXPORT_SYMBOL(blocking_thread_register_notify);
+EXPORT_SYMBOL(atomic_thread_register_notify);
 /* Additional variables */
 EXPORT_SYMBOL(__phys_offset);
 EXPORT_SYMBOL(_dflt_cache_att);
@@ -35,8 +39,17 @@ EXPORT_SYMBOL(_dflt_cache_att);
 DECLARE_EXPORT(__hexagon_memcpy_likely_aligned_min32bytes_mult8bytes);
 
 /* Additional functions */
+DECLARE_EXPORT(csum_tcpudp_magic);
+
+/* libgcc symbol names changes */
+#if (defined(CONFIG_HEXAGON_LLVM) || (3 == __GNUC__) || (4 == __GNUC__ && 4 == __GNUC_MINOR__)) || (__clang__ == 1)
+DECLARE_EXPORT(__hexagon_divsi3);
+DECLARE_EXPORT(__hexagon_modsi3);
+DECLARE_EXPORT(__hexagon_udivsi3);
+DECLARE_EXPORT(__hexagon_umodsi3);
+#else
 DECLARE_EXPORT(__divsi3);
 DECLARE_EXPORT(__modsi3);
 DECLARE_EXPORT(__udivsi3);
 DECLARE_EXPORT(__umodsi3);
-DECLARE_EXPORT(csum_tcpudp_magic);
+#endif
