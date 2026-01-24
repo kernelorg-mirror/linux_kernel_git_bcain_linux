fa562ee2c0a89d322f5ac11dbceafa6bb4e86922
Brian Cain <brian.cain@oss.qualcomm.com>
2026-01-24 16:35:20 -0600

    hexagon: add Angel console support
    Add Angel semihosting console support for early boot output
    and debugging on hexagon platforms.
    
    Signed-off-by: Richard Kuo <rkuo@quicinc.com>
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

---
+++ arch/hexagon/include/asm/angel_console.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2010-2011, Code Aurora Forum. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#ifndef _ANGEL_CONSOLE_H
+#define _ANGEL_CONSOLE_H
+
+struct console;
+
+extern unsigned int on_simulator;
+
+void __init register_angel_console(void);
+
+void angel_write(struct console *c, const char *s, unsigned n);
+
+void __init angel_get_command_line(char *s, unsigned n);
+
+#endif
