d27b905c1b3f7c5d8c2dc6d41fae9a259062d4c7
Brian Cain <brian.cain@oss.qualcomm.com>
2026-01-24 16:38:48 -0600

    hexagon: update cache headers
    Update cache and cache flush headers.
    
    Signed-off-by: Richard Kuo <rkuo@quicinc.com>
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- arch/hexagon/include/asm/cache.h
+++ arch/hexagon/include/asm/cache.h
@@ -8,11 +8,14 @@
 #ifndef __ASM_CACHE_H
 #define __ASM_CACHE_H
 
+
+#define L2_PREFETCH_BYTES	(128)
+
 /* Bytes per L1 cache line */
 #define L1_CACHE_SHIFT		(5)
 #define L1_CACHE_BYTES		(1 << L1_CACHE_SHIFT)
 
-#define ARCH_DMA_MINALIGN	L1_CACHE_BYTES
+#define ARCH_DMA_MINALIGN	L2_PREFETCH_BYTES
 
 #define __cacheline_aligned	__aligned(L1_CACHE_BYTES)
 #define ____cacheline_aligned	__aligned(L1_CACHE_BYTES)
