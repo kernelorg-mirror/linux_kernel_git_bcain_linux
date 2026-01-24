9fd0493bb01f8e691dff307cf5b664c14bde1b85
Brian Cain <brian.cain@oss.qualcomm.com>
2026-01-24 16:38:42 -0600

    hexagon: update memory management headers
    Update memory layout, MMU context, page table, and page allocation
    headers for improved memory management support.
    
    Signed-off-by: Richard Kuo <rkuo@quicinc.com>
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- arch/hexagon/include/asm/pgalloc.h
+++ arch/hexagon/include/asm/pgalloc.h
@@ -35,7 +35,7 @@ static inline pgd_t *pgd_alloc(struct mm_struct *mm)
 {
 	pgd_t *pgd;
 
-	pgd = (pgd_t *)__get_free_page(GFP_KERNEL | __GFP_ZERO);
+	pgd = (pgd_t *)__get_free_page(GFP_KERNEL);
 
 	/*
 	 * There may be better ways to do this, but to ensure
@@ -51,6 +51,9 @@ static inline pgd_t *pgd_alloc(struct mm_struct *mm)
 	/* Physical version is what is passed to virtual machine on switch */
 	mm->context.ptbase = __pa(pgd);
 
+	/*  Always invalidate at switch the first time a base is allocated  */
+	mm->context.need_invalidate = true;
+
 	return pgd;
 }
 
@@ -63,13 +66,21 @@ static inline struct page *pte_alloc_one(struct mm_struct *mm,
 					 unsigned long address)
 {
 	struct page *pte;
+	pte_t *start;
+	int i;
 
-	pte = alloc_page(GFP_KERNEL | __GFP_REPEAT | __GFP_ZERO);
+	pte = alloc_page(GFP_KERNEL | __GFP_REPEAT);
 	if (!pte)
 		return NULL;
 	if (!pgtable_page_ctor(pte)) {
 		__free_page(pte);
 		return NULL;
+	}
+	//  need to initialize to _NULL_PTE, or fine tune the macros to differentiate PMD's from PTE's.
+	start = page_to_virt(pte);
+	for (i=0; i < PTRS_PER_PTE; i++) {
+		// feels weird to use pte_clear
+		pte_val(*start++) = _NULL_PTE;
 	}
 	return pte;
 }
@@ -78,8 +89,22 @@ static inline struct page *pte_alloc_one(struct mm_struct *mm,
 static inline pte_t *pte_alloc_one_kernel(struct mm_struct *mm,
 					  unsigned long address)
 {
-	gfp_t flags =  GFP_KERNEL | __GFP_REPEAT | __GFP_ZERO;
-	return (pte_t *) __get_free_page(flags);
+	pte_t *pte;
+	pte_t *start;
+	int i;
+
+	gfp_t flags =  GFP_KERNEL | __GFP_REPEAT;
+
+	pte = __get_free_page(flags);
+
+	//  need to initialize to _NULL_PTE
+	start = pte;
+	for (i=0; i < PTRS_PER_PTE; i++) {
+		// feels weird to use pte_clear
+		pte_val(*start++) = _NULL_PTE;
+	}
+
+	return pte;
 }
 
 static inline void pte_free(struct mm_struct *mm, struct page *pte)
