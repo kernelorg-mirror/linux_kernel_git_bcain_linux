4420ba219f5fcabfce7ba0ecf521f56024aab6d4
Brian Cain <brian.cain@oss.qualcomm.com>
2026-01-24 16:35:07 -0600

    hexagon: add platform infrastructure
    Add core platform infrastructure including machine descriptor for
    board registration and platform initialization framework.
    
    Co-authored-by: Richard Kuo <rkuo@quicinc.com>
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

---
+++ arch/hexagon/include/asm/platform.h
@@ -0,0 +1,31 @@
+/*
+ * Platform support for the Hexagon architecture
+ *
+ * Copyright (c) 2010-2011, Code Aurora Forum. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#ifndef __PLATFORM_H
+#define __PLATFORM_H
+
+#include <asm/mach_desc.h>
+
+extern unsigned long bootmem_lastpg;
+extern u32 dt_blob_start;
+
+extern void early_memtest(unsigned long start, unsigned long end);
+
+#endif
