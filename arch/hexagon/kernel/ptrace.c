a68a8ae0074a17b7389ba3f8d0fd6b97c0b578de
Brian Cain <brian.cain@oss.qualcomm.com>
2026-01-24 16:39:54 -0600

    hexagon: update ptrace support
    Update ptrace debugging support with improved register access.
    
    Signed-off-by: Richard Kuo <rkuo@quicinc.com>
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- arch/hexagon/kernel/ptrace.c
+++ arch/hexagon/kernel/ptrace.c
@@ -28,8 +28,8 @@
 #include <linux/regset.h>
 #include <linux/user.h>
 #include <linux/elf.h>
-
 #include <asm/user.h>
+#include <asm/hvx.h>
 
 #if arch_has_single_step()
 /*  Both called from ptrace_resume  */
@@ -65,6 +65,9 @@ static int genregs_get(struct task_struct *target,
 	 * sequence in the kernel, so everything past the 32 gprs
 	 * happens one at a time.
 	 */
+
+	/*  To-Do:  sanitize the calling args somewhere  */
+
 	ret = user_regset_copyout(&pos, &count, &kbuf, &ubuf,
 				  &regs->r00, 0, 32*sizeof(unsigned long));
 
@@ -90,10 +93,8 @@ static int genregs_get(struct task_struct *target,
 	dummy = pt_cause(regs);
 	ONEXT(&dummy, cause);
 	ONEXT(&pt_badva(regs), badva);
-#if CONFIG_HEXAGON_ARCH_VERSION >=4
 	ONEXT(&regs->cs0, cs0);
 	ONEXT(&regs->cs1, cs1);
-#endif
 
 	/* Pad the rest with zeros, if needed */
 	if (!ret)
@@ -141,10 +142,8 @@ static int genregs_set(struct task_struct *target,
 	INEXT(&bucket, cause);
 	INEXT(&bucket, badva);
 
-#if CONFIG_HEXAGON_ARCH_VERSION >=4
 	INEXT(&regs->cs0, cs0);
 	INEXT(&regs->cs1, cs1);
-#endif
 
 	/* Ignore the rest, if needed */
 	if (!ret)
@@ -162,8 +161,54 @@ static int genregs_set(struct task_struct *target,
 	return 0;
 }
 
+//  Any way to move these to the module?
+static int fpregs_get(struct task_struct *target,
+		   const struct user_regset *regset,
+		   unsigned int pos, unsigned int count,
+		   void *kbuf, void __user *ubuf)
+{
+	int ret;
+	struct thread_info *ti = task_thread_info(target);
+
+	if (!ti->hvx)
+		return -EIO;
+
+	/* 32 vector regs + 1 vecpredregs  == 33 */
+	ret = user_regset_copyout(&pos, &count, &kbuf, &ubuf,
+				  ti->hvx->vregs, 0, 33*sizeof(HVX_Vector));
+
+	return ret;
+}
+
+static int fpregs_set(struct task_struct *target,
+		   const struct user_regset *regset,
+		   unsigned int pos, unsigned int count,
+		   const void *kbuf, const void __user *ubuf)
+{
+	int ret;
+	struct thread_info *ti = task_thread_info(target);
+
+	if (!ti->hvx)
+		return -EIO;
+
+	/* 32 vector regs + 1 vecpredregs  == 33 */
+	ret = user_regset_copyin(&pos, &count, &kbuf, &ubuf,
+				 ti->hvx->vregs, 0, 33*sizeof(HVX_Vector));
+
+	return ret;
+}
+
+static int fpregs_active(struct task_struct *target,
+                         const struct user_regset *regset)
+{
+	struct thread_info *thread = task_thread_info(target);
+
+	return thread->hvx;
+}
+
 enum hexagon_regset {
 	REGSET_GENERAL,
+	REGSET_FPU,
 };
 
 static const struct user_regset hexagon_regsets[] = {
@@ -175,6 +220,16 @@ static const struct user_regset hexagon_regsets[] = {
 		.get = genregs_get,
 		.set = genregs_set,
 	},
+	[REGSET_FPU] = {
+                .core_note_type = NT_PRFPREG,
+                .n              = sizeof(struct user_fpregs_struct) /
+                                  sizeof(HVX_Vector),
+                .size           = sizeof(HVX_Vector),
+                .align          = sizeof(HVX_Vector),
+                .get            = fpregs_get,
+                .set            = fpregs_set,
+                .active         = fpregs_active,
+	},
 };
 
 static const struct user_regset_view hexagon_user_view = {
@@ -182,7 +237,6 @@ static const struct user_regset_view hexagon_user_view = {
 	.e_machine = ELF_ARCH,
 	.ei_osabi = ELF_OSABI,
 	.regsets = hexagon_regsets,
-	.e_flags = ELF_CORE_EFLAGS,
 	.n = ARRAY_SIZE(hexagon_regsets)
 };
 
