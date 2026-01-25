04de7dc1d8174d415fa5f19eb718aa6d9f804169
Brian Cain <brian.cain@oss.qualcomm.com>
2026-01-24 22:12:10 -0600

    hexagon: fix v4.13 compatibility issues
    - Remove duplicate dma_supported/dma_is_consistent/dma_cache_sync
      declarations from asm/dma-mapping.h (now provided by linux/dma-mapping.h)
    - Replace __GFP_REPEAT with __GFP_RETRY_MAYFAIL in pgalloc.h
    - Add sz parameter to huge_pte_offset() in hugetlbpage.c
    - Add linux/io.h include to v66_debug.c for readl/writel/ioremap

--- arch/hexagon/include/asm/pgalloc.h
+++ arch/hexagon/include/asm/pgalloc.h
@@ -55,7 +55,7 @@ static inline struct page *pte_alloc_one(struct mm_struct *mm)
 	pte_t *start;
 	int i;
 
-	pte = alloc_page(GFP_KERNEL | __GFP_REPEAT);
+	pte = alloc_page(GFP_KERNEL | __GFP_RETRY_MAYFAIL);
 	if (!pte)
 		return NULL;
 	if (!pgtable_page_ctor(pte)) {
@@ -78,7 +78,7 @@ static inline pte_t *pte_alloc_one_kernel(struct mm_struct *mm)
 	pte_t *start;
 	int i;
 
-	gfp_t flags =  GFP_KERNEL | __GFP_REPEAT;
+	gfp_t flags =  GFP_KERNEL | __GFP_RETRY_MAYFAIL;
 
 	pte = (pte_t *)__get_free_page(flags);
 
