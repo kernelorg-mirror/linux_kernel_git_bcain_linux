e04c2b8720fc6597335bfef795a608aafe7d0e8d
Brian Cain <brian.cain@oss.qualcomm.com>
2026-02-17 08:59:21 -0800

    hexagon: support sub-word cmpxchg and xchg
    The Hexagon architecture only has word-sized LL/SC instructions
    (memw_locked), but arch_cmpxchg and arch_xchg were only implemented
    for 32-bit operands.  A cmpxchg on a u8 or u16 would emit memw_locked
    at the operand's address, faulting if that address is not 4-byte
    aligned.
    
    This caused a Misaligned Data Load exception in exit_tasks_rcu_finish
    when CONFIG_TASKS_TRACE_RCU is enabled (selected by CONFIG_BPF_SYSCALL),
    because rcu_trc_cmpxchg_need_qs() performs cmpxchg on the u8 field
    trc_reader_special.b.need_qs, which sits at a non-4-byte-aligned offset
    within task_struct.
    
    Add __cmpxchg_small and __xchg_small helpers that align the pointer
    down to the containing 4-byte word, use memw_locked on that aligned
    address, and mask/shift to operate on just the target byte(s) within
    the word.  This follows the same approach used by LoongArch, RISC-V,
    and MIPS for sub-word atomics.
    
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- arch/hexagon/include/asm/cmpxchg.h
+++ arch/hexagon/include/asm/cmpxchg.h
@@ -8,34 +8,69 @@
 #ifndef _ASM_CMPXCHG_H
 #define _ASM_CMPXCHG_H
 
+#include <linux/build_bug.h>
+
 /*
- * __arch_xchg - atomically exchange a register and a memory location
- * @x: value to swap
- * @ptr: pointer to memory
- * @size:  size of the value
- *
- * Only 4 bytes supported currently.
- *
- * Note:  there was an errata for V2 about .new's and memw_locked.
- *
+ * __xchg_word - atomically exchange a 32-bit register and memory location
  */
 static inline unsigned long
-__arch_xchg(unsigned long x, volatile void *ptr, int size)
+__xchg_word(unsigned long x, volatile void *ptr)
 {
 	unsigned long retval;
 
-	/*  Can't seem to use printk or panic here, so just stop  */
-	if (size != 4) do { asm volatile("brkpt;\n"); } while (1);
-
 	__asm__ __volatile__ (
-	"1:	%0 = memw_locked(%1);\n"    /*  load into retval */
-	"	memw_locked(%1,P0) = %2;\n" /*  store into memory */
+	"1:	%0 = memw_locked(%1);\n"
+	"	memw_locked(%1,P0) = %2;\n"
 	"	if (!P0) jump 1b;\n"
 	: "=&r" (retval)
 	: "r" (ptr), "r" (x)
 	: "memory", "p0"
 	);
 	return retval;
+}
+
+/*
+ * __xchg_small - atomically exchange a sub-word (1 or 2 byte) value.
+ *
+ * Hexagon only has word-sized LL/SC (memw_locked), so we align down to
+ * the containing word, load it with LL, replace the target byte(s),
+ * and store back with SC.
+ */
+static inline unsigned long
+__xchg_small(unsigned long x, volatile void *ptr, int size)
+{
+	unsigned long aligned, shift, mask, old32, new32;
+
+	aligned = (unsigned long)ptr & ~3UL;
+	shift = ((unsigned long)ptr & 3) * 8;
+	mask = ((1UL << (size * 8)) - 1) << shift;
+	x = (x << shift) & mask;
+
+	__asm__ __volatile__ (
+	"1:	%0 = memw_locked(%2);\n"
+	"	%1 = and(%0, %4);\n"		/* clear target bits */
+	"	%1 = or(%1, %3);\n"		/* insert new value */
+	"	memw_locked(%2, P0) = %1;\n"
+	"	if (!P0) jump 1b;\n"
+	: "=&r" (old32), "=&r" (new32)
+	: "r" (aligned), "r" (x), "r" (~mask)
+	: "memory", "p0"
+	);
+	return (old32 & mask) >> shift;
+}
+
+static inline unsigned long
+__arch_xchg(unsigned long x, volatile void *ptr, int size)
+{
+	switch (size) {
+	case 1:
+	case 2:
+		return __xchg_small(x, ptr, size);
+	case 4:
+		return __xchg_word(x, ptr);
+	default:
+		BUILD_BUG();
+	}
 }
 
 /*
@@ -46,30 +81,91 @@ __arch_xchg(unsigned long x, volatile void *ptr, int size)
 							   sizeof(*(ptr))))
 
 /*
+ * __cmpxchg_word - atomic compare-and-exchange for 32-bit values.
+ */
+static inline unsigned long
+__cmpxchg_word(volatile void *ptr, unsigned long old, unsigned long new)
+{
+	unsigned long oldval;
+
+	__asm__ __volatile__ (
+	"1:	%0 = memw_locked(%1);\n"
+	"	{ P0 = cmp.eq(%0,%2);\n"
+	"	  if (!P0.new) jump:nt 2f; }\n"
+	"	memw_locked(%1,p0) = %3;\n"
+	"	if (!P0) jump 1b;\n"
+	"2:\n"
+	: "=&r" (oldval)
+	: "r" (ptr), "r" (old), "r" (new)
+	: "memory", "p0"
+	);
+	return oldval;
+}
+
+/*
+ * __cmpxchg_small - atomic compare-and-exchange for sub-word (1/2 byte) values.
+ *
+ * Hexagon only has word-sized LL/SC (memw_locked), so we align down to
+ * the containing word, load it with LL, compare/replace the target byte(s)
+ * within the word, and store back with SC.
+ */
+static inline unsigned long
+__cmpxchg_small(volatile void *ptr, unsigned long old, unsigned long new,
+		int size)
+{
+	unsigned long aligned, shift, mask, old32;
+	unsigned long old_shifted, new_shifted, tmp;
+
+	aligned = (unsigned long)ptr & ~3UL;
+	shift = ((unsigned long)ptr & 3) * 8;
+	mask = ((1UL << (size * 8)) - 1) << shift;
+	old_shifted = (old << shift) & mask;
+	new_shifted = (new << shift) & mask;
+
+	__asm__ __volatile__ (
+	"1:	%0 = memw_locked(%2);\n"
+	"	%1 = and(%0, %3);\n"		/* extract target bits */
+	"	{ P0 = cmp.eq(%1,%4);\n"
+	"	  if (!P0.new) jump:nt 2f; }\n"
+	"	%1 = and(%0, %5);\n"		/* clear target bits */
+	"	%1 = or(%1, %6);\n"		/* insert new value */
+	"	memw_locked(%2, P0) = %1;\n"
+	"	if (!P0) jump 1b;\n"
+	"2:\n"
+	: "=&r" (old32), "=&r" (tmp)
+	: "r" (aligned), "r" (mask), "r" (old_shifted),
+	  "r" (~mask), "r" (new_shifted)
+	: "memory", "p0"
+	);
+	return (old32 & mask) >> shift;
+}
+
+static inline unsigned long
+__arch_cmpxchg(volatile void *ptr, unsigned long old, unsigned long new,
+	       int size)
+{
+	switch (size) {
+	case 1:
+	case 2:
+		return __cmpxchg_small(ptr, old, new, size);
+	case 4:
+		return __cmpxchg_word(ptr, old, new);
+	default:
+		BUILD_BUG();
+	}
+}
+
+/*
  *  see rt-mutex-design.txt; cmpxchg supposedly checks if *ptr == A and swaps.
  *  looks just like atomic_cmpxchg on our arch currently with a bunch of
  *  variable casting.
  */
-
-#define arch_cmpxchg(ptr, old, new)				\
-({								\
-	__typeof__(ptr) __ptr = (ptr);				\
-	__typeof__(*(ptr)) __old = (old);			\
-	__typeof__(*(ptr)) __new = (new);			\
-	__typeof__(*(ptr)) __oldval = (__typeof__(*(ptr))) 0;	\
-								\
-	asm volatile(						\
-		"1:	%0 = memw_locked(%1);\n"		\
-		"	{ P0 = cmp.eq(%0,%2);\n"		\
-		"	  if (!P0.new) jump:nt 2f; }\n"		\
-		"	memw_locked(%1,p0) = %3;\n"		\
-		"	if (!P0) jump 1b;\n"			\
-		"2:\n"						\
-		: "=&r" (__oldval)				\
-		: "r" (__ptr), "r" (__old), "r" (__new)		\
-		: "memory", "p0"				\
-	);							\
-	__oldval;						\
+#define arch_cmpxchg(ptr, old, new)					\
+({									\
+	(__typeof__(*(ptr)))__arch_cmpxchg((ptr),			\
+					  (unsigned long)(old),		\
+					  (unsigned long)(new),		\
+					  sizeof(*(ptr)));		\
 })
 
 #endif /* _ASM_CMPXCHG_H */
