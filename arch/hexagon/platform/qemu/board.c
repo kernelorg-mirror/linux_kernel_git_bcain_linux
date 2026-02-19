6df2a55a41a41b0ea3c34df89e2c0e6ba966f72d
Brian Cain <brian.cain@oss.qualcomm.com>
2026-02-18 17:56:36 -0800

    hexagon: add QEMU virt machine platform with dedicated device tree
    Add a new CONFIG_HEXAGON_QEMU platform for the QEMU Hexagon virtual
    machine, separating it from the SM8150 platform. This includes:
    
    - New Kconfig entry CONFIG_HEXAGON_QEMU with appropriate selects
    - New platform directory arch/hexagon/platform/qemu/ with board init
    - New qemu_virt.dts with "qcom,hexagon-virt" compatible string,
      PL011 as primary UART (serial0/ttyAMA0) and QUP GENI as secondary
      (serial1/ttyHS1), 4 CPU nodes, and virtio devices
    - Revert QUP GENI nodes from sm8150.dts back to PL011-only
    - Update qemu_defconfig to use CONFIG_HEXAGON_QEMU
    
    The QUP GENI UART uses "qcom,geni-uart" (non-console) compatible
    to avoid the GENI_UART_CONS_PORTS=1 line number limitation when
    assigned as serial1.
    
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

---
+++ arch/hexagon/platform/qemu/board.c
@@ -0,0 +1,57 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0
+ * Board support for QEMU Hexagon virtual machine
+ *
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+/*
+ * Board support for QEMU Hexagon virtual machine
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/platform_device.h>
+#include <linux/of_platform.h>
+#include <asm/irq.h>
+#include <asm/platform.h>
+#include <asm/platform/sirc.h>
+
+static const char *qemu_dt_compat[] __initconst = {
+	"qcom,hexagon-virt",
+	NULL
+};
+
+static struct platform_device *devices[] __initdata = {
+};
+
+const struct of_device_id platform_of_irq_matches[] __initdata = {
+	{ .compatible = "qcom,h2-pic", .data = hexagon_pic_of_init, },
+	{},
+};
+
+static int __init qemu_init(void)
+{
+	pr_info("%s\n", __func__);
+
+	if (strcmp(mdesc->name, "hexagon-qemu"))
+		return 0;
+
+	platform_add_devices(devices, ARRAY_SIZE(devices));
+	of_platform_populate(of_find_node_by_path("/soc"), NULL, NULL, NULL);
+
+	return 0;
+}
+
+arch_initcall(qemu_init);
+
+static void setup_arch_platform_qemu(void)
+{
+	pr_info("%s\n", __func__);
+	bootmem_lastpg = PFN_DOWN(1 << 26);
+}
+
+MACHINE_START(HEXAGON_QEMU, "hexagon-qemu")
+	.setup_arch_platform = setup_arch_platform_qemu,
+	.dt_compat = qemu_dt_compat
+MACHINE_END
