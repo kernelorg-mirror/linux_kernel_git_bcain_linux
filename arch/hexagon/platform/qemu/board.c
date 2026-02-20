3f65c67d57ca637662e19a6955b9f16ee3fee799
Brian Cain <brian.cain@oss.qualcomm.com>
2026-02-19 16:30:22 -0800

    hexagon: use cpumask_of() for clock event device registration
    clockevents_register_device() warns when cpumask is set to
    cpu_all_mask. Use cpumask_of(smp_processor_id()) to specify the
    boot CPU as the target for the clock event device.
    
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- arch/hexagon/platform/qemu/board.c
+++ arch/hexagon/platform/qemu/board.c
@@ -25,7 +25,7 @@ static const char *qemu_dt_compat[] __initconst = {
 static struct platform_device *devices[] __initdata = {
 };
 
-const struct of_device_id platform_of_irq_matches[] __initdata = {
+const struct of_device_id platform_of_irq_matches[] = {
 	{ .compatible = "qcom,h2-pic", .data = hexagon_pic_of_init, },
 	{},
 };
