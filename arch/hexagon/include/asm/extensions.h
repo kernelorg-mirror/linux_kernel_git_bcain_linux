f81eb60319a945d44a95f0d218113828e9e79d8c
Richard Kuo <rkuo@quicinc.com>
2026-04-27 20:00:20 -0700

    hexagon: add HMX coprocessor context management
    Add the HMX (Hexagon Matrix eXtension) context management module.  HMX
    uses a lightweight enable/disable model via HWCONFIG_HMXBITS rather than
    full register save/restore: on context switch, HMX hardware access is
    enabled for threads that have faulted on a coprocessor instruction and
    disabled otherwise.
    
    The thread notification framework (asm/notify.h) hooks coprocessor faults
    to lazily allocate per-thread HMX state, and restore-all events to toggle
    hardware access.  A sysfs class interface exposes refcount, debug, and
    manual release controls.
    
    Also adds HWCONFIG_HMXBITS to the hwconfig enum and extends struct
    extinfo with an hmx pointer alongside hvx.
    
    Signed-off-by: Richard Kuo <rkuo@quicinc.com>
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- arch/hexagon/include/asm/extensions.h
+++ arch/hexagon/include/asm/extensions.h
@@ -1,5 +1,8 @@
 /* Arch extensions - stub when HVX is not available */
+#include <asm/hmx.h>
 #include <asm/hvx.h>
 
 struct extinfo {
+	struct hvx_threadinfo *hvx;
+	struct hmx_threadinfo *hmx;
 };
